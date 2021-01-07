6741d5f977879b8e618ba81744dfcf4fddd66ee9
YuzhouChang <cyz931123@gmail.com>
2021-01-07 18:22:40 -0500

    Update LTMGSCA.R

--- R/LTMGSCA.R
+++ R/LTMGSCA.R
@@ -7,7 +7,7 @@
 InverseMillsRatio <- function(q, mean, sd) {
     x <- (q - mean)/sd
     pdf <- dnorm(x, log = TRUE)
-    cdf <- pnorm(x, log = TRUE)
+    cdf <- pnorm(x, log.p = TRUE)
     d <- exp(pdf - cdf)
     d[is.na(d)] <- 0
     return(d)
